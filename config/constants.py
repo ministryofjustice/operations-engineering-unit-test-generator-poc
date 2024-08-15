ee5b97ec2b952d015bcffb4746423f9ffa9f66f6
levgorbunov1 <lev.gorbunov@digital.justice.gov.uk>
2024-08-15 17:30:12 +0100

    add core script infrastructure

---
+++ config/constants.py
@@ -0,0 +1,22 @@
+# class Constants:
+MINISTRY_OF_JUSTICE = "ministryofjustice"
+MOJ_ANALYTICAL_SERVICES = "moj-analytical-services"
+MISSING_EMAIL_ADDRESS = "-"
+NO_ACTIVITY = "no activity"
+ENTERPRISE = "ministry-of-justice-uk"
+SLACK_CHANNEL = "operations-engineering-alerts"
+SR_SLACK_CHANNEL = "operations-engineering-team"
+OPERATIONS_ENGINEERING_GITHUB_USERNAMES = [
+    "SteveMarshall",
+    "andyrogers1973",
+    "jasonBirchall",
+    "tamsinforbes",
+    "AntonyBishop",
+    "connormaglynn",
+    "PepperMoJ",
+    "levgorbunov1",
+    "vijaykannan21",
+    "moj-operations-engineering-bot",
+]
+LLAMA_MODEL_ID = "meta.llama3-70b-instruct-v1:0"
+CLAUDE_MODEL_ID = "anthropic.claude-3-sonnet-20240229-v1:0"
---
+++ prompt_template.py
@@ -0,0 +1,77 @@
+COMMON_RULES = """
+Rules:
+- Tests must be written in Python using the unittest framework.
+- Do not acknowledge the question asked.
+- Do not include any extra information.
+- Do not explain your answers.
+- Only produce the test code, nothing else.
+- Make sure that you are importing classes and function into the test script in the correct way,
+for example, if I want to import the class "MyService" from the module "services.my_service" I would do it this way:
+"from services.my_service import MyService".
+- Do not include any markdown in the output e.g. ```python something ```
+- Ensure constants are correctly imported: "from config.constants import MY_CONSTANT"
+"""
+
+NEW_TEST_SUITE_PROMPT_TEMPLATE = """
+
+Please write a new test suite, using the Python unittest framework, for the provided script:
+
+{file_to_test_content}
+
+Classes and functions should be imported into the test script from the following module reference: {module}
+
+For example. given this script:
+
+{example_script}
+
+This is the associated test suite:
+
+{example_test_suite}
+""" + COMMON_RULES
+
+MODIFY_TEST_SUITE_PROMPT_TEMPLATE = """
+
+This a a python script:
+
+{file_to_test_content}
+
+This is the current unit test suite for this script:
+
+{unit_test_file_content}
+
+Please update the unit tests for the following functions which have been recently modified:
+{modified_function_names}
+
+Do not modify any existing unit tests unless specified.
+
+Please add tests to the current suite for functions in the script that do not have unit tests.
+
+Do not create additional unit tests for functions that are already associated with unit tests.
+
+Please respond with the complete new unit test suite.
+
+For example. given this script:
+
+{example_script}
+
+This is the associated test suite:
+
+{example_test_suite}
+""" + COMMON_RULES
+
+FAILED_TESTS_PROMPT_TEMPLATE = """
+This is the script that I am testing:
+
+{file_to_test_content}
+
+This is the current unit test suite for this script:
+
+{unit_test_file_content}
+
+These are the current unit test failures:
+{failed_tests}
+
+Please amend the failing tests so that they pass and return the new test suite.
+
+Use mock_response.__getitem__.return_value to mock response["body"].
+""" + COMMON_RULES
